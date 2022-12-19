fa8b5d8c88a1d07887622f55af97eb05cbc564ea
Ram Ratan Maurya <ratan.maurya@gmail.com>
2022-12-19 09:26:55 +0530

    Initial Commit

---
+++ Lemon Squeezy/Lemon_SqueezyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lemon_SqueezyApp.swift
+//  Lemon Squeezy
+//
+//  Created by Ram Ratan Maurya on 19/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Lemon_SqueezyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
